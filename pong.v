29761c2eb237498bb221731dd5f8c0cd8431935e
jsheppard92 <jwsheppa@usc.edu>
2017-04-28 23:53:57 -0700

    Initial commit

---
+++ pong.v
@@ -0,0 +1,197 @@
+`timescale 1ns / 1ps
+module pong(MemOE, MemWR, RamCS, FlashCS, QuadSpiFlashCS, // Disable the three memory chips
+	ClkPort, 		// 100 MHz
+	vga_h_sync, vga_v_sync, vga_r, vga_g, vga_b,  // VGA signals
+	btnU, btnD, btnL, btnC, btnR,
+	An0, An1, An2, An3, 
+	Ca, Cb, Cc, Cd, Ce, Cf, Cg, 
+	Ld0, Ld1, Ld2, Ld3, Ld4, Ld5, Ld6, Ld7,
+	JA0, JA1, JA2, JA3);
+	
+	
+	output 	MemOE, MemWR, RamCS, FlashCS, QuadSpiFlashCS;
+	input ClkPort, btnU, btnD, btnL, btnR, btnC;
+	input JA0, JA1, JA2, JA3;
+	output vga_h_sync, vga_v_sync, vga_r, vga_g, vga_b;
+	output An0, An1, An2, An3, Ca, Cb, Cc, Cd, Ce, Cf, Cg;
+	output Ld0, Ld1, Ld2, Ld3, Ld4, Ld5, Ld6, Ld7;
+	reg vga_r, vga_g, vga_b;
+	
+	 
+	// Disable the three memories so that they do not interfere with the rest of the design.
+	assign {MemOE, MemWR, RamCS, FlashCS, QuadSpiFlashCS} = 5'b11111;
+	
+	
+	/*  LOCAL SIGNALS */
+	wire	reset, ClkPort, board_clk, clk, ack;
+	
+	BUF BUF1 (board_clk, ClkPort); 	
+	BUF BUF2 (reset, btnC);
+	BUF BUF3 (ack, btnR);
+	BUF BUF4	(start, btnL);
+	
+	reg [27:0]	DIV_CLK;
+	always @ (posedge board_clk, posedge reset)  
+	begin : CLOCK_DIVIDER
+      if (reset)
+			DIV_CLK <= 0;
+      else
+			DIV_CLK <= DIV_CLK + 1'b1;
+	end	
+
+	assign	clk = DIV_CLK[1];
+	assign	sm_clk = DIV_CLK[20]; 
+	
+	wire inDisplayArea;
+	wire [9:0] CounterX;
+	wire [9:0] CounterY;
+	
+	//////////////////
+	
+	wire [9:0] x;
+	wire [9:0] y;
+	wire [9:0] text_offset;
+	reg [9:0] p_1;
+	reg [9:0] p_2;
+	wire [3:0] player_1_score;
+	wire [3:0] player_2_score;
+	
+	//////////////////
+
+	hvsync_generator syncgen(.clk(clk), .reset(reset),.vga_h_sync(vga_h_sync), .vga_v_sync(vga_v_sync), .inDisplayArea(inDisplayArea), .CounterX(CounterX), .CounterY(CounterY));
+	pong_sm psm(.clk(sm_clk), .reset(reset), .start(start), .ack(ack), .x(x), .y(y),.text_offset(text_offset), .player_1_score(player_1_score), .player_2_score(player_2_score), .p_1(p_1), .p_2(p_2), .q_INITIAL(q_INITIAL), .q_SERVE(q_SERVE), .q_MOVE(q_MOVE), .q_CHECK(q_CHECK), .q_SCORE(q_SCORE), .q_GAMEOVER(q_GAMEOVER));
+	
+	/////////////////////////////////////////////////////////////////
+	///////////////		VGA control starts here		/////////////////
+	/////////////////////////////////////////////////////////////////
+	reg [9:0] position;
+	
+	
+	always @(posedge DIV_CLK[19])
+		begin
+			if(reset || ack)
+			begin
+				p_1 <= 240;
+				p_2 <= 240;
+			end
+			else if( (JA3 | JA1) && ~(JA2 && JA0) && (p_1 > 40) )
+				begin
+					p_1 <= p_1 - 5;
+				end
+			else if( (JA2 | JA0) && ~(JA3 && JA1) && (p_1 < 440))
+				begin
+					p_1 <= p_1 + 5;
+				end
+			else if(btnU && ~btnD && (p_2 > 40))
+				begin
+					p_2 <= p_2 - 5;
+				end
+			else if(btnD && ~btnU && (p_2 < 440))
+				begin
+					p_2 <= p_2 + 5;
+				end
+		end
+
+	wire ball = ( (CounterX>=x-3) && (CounterX<=x+3) && (CounterY>=y-1) && (CounterY<=y+1) ) || ( (CounterX>=x-2) && (CounterX<=x+2) && ( (CounterY==y-2) || (CounterY==y+2) ))  || ( (CounterX>=x-1) && (CounterX<=x+1) && ((CounterY==y-3) || (CounterY==y+3)));
+	
+	wire left_paddle = ( ( CounterX>=0 ) && ( CounterX<=15 ) && ( CounterY<=(p_1+40) ) && ( CounterY>=(p_1-40) ) ) ;
+	
+	wire right_paddle = ( ( CounterX>625 ) && ( CounterX<=640 ) && ( CounterY<=(p_2+40) ) && ( CounterY>=(p_2-40) ) ) ;
+	
+	wire w = ( ( (CounterX >= (40+text_offset)) && (CounterX < (50+text_offset)) ) && ( (CounterY >= 40) && (CounterY < 80) ) || ( (CounterX >= (50+text_offset)) && (CounterX < (60+text_offset)) ) && ( (CounterY >= 80) && (CounterY < 90) )
+				|| ( ((CounterX >= (60+text_offset)) && (CounterX < (70+text_offset))) && ( (CounterY >= 63) && (CounterY < 80) )  || ( (CounterX >= (70+text_offset)) && (CounterX < (80+text_offset) ) && ( (CounterY >= 80) && (CounterY < 90) )
+				|| ( (CounterX >= (80+text_offset)) && (CounterX < (90+text_offset)) ) && ( (CounterY >= 40) && (CounterY < 80) ) )));
+	wire l = ( (( (CounterX >= (570-text_offset)) && (CounterX < (580-text_offset)) )) && ( (CounterY >= 40) && (CounterY < 90)) ) || (( (CounterX >= (580-text_offset)) && (CounterX < (600-text_offset)) ) && ( (CounterY >= 80) && (CounterY < 90)));
+	
+	always @(posedge clk)
+	begin
+		vga_r <= ( ball | left_paddle | (l & q_GAMEOVER)) & inDisplayArea;
+		vga_g <= (ball | (CounterX >= 319 && CounterX <= 321 && (CounterY % 10)) | (w & q_GAMEOVER))  & inDisplayArea;
+		vga_b <= ( right_paddle | (CounterX >= 319 && CounterX <= 321 && (CounterY % 10)) ) & inDisplayArea;
+	end
+	
+	/////////////////////////////////////////////////////////////////
+	//////////////  	  VGA control ends here 	 ///////////////////
+	/////////////////////////////////////////////////////////////////
+	
+	/////////////////////////////////////////////////////////////////
+	//////////////  	  LD control starts here 	 ///////////////////
+	/////////////////////////////////////////////////////////////////
+	
+
+	assign {Ld7, Ld6, Ld5, Ld4} = {0, 0, q_GAMEOVER, q_SCORE}; 
+	assign {Ld3, Ld2, Ld1, Ld0} = {q_CHECK, q_MOVE, q_SERVE, q_INITIAL}; 
+	
+	
+	/////////////////////////////////////////////////////////////////
+	//////////////  	  LD control ends here 	 	////////////////////
+	/////////////////////////////////////////////////////////////////
+	
+	/////////////////////////////////////////////////////////////////
+	//////////////  	  SSD control starts here 	 ///////////////////
+	/////////////////////////////////////////////////////////////////
+	reg 	[3:0]	SSD;
+	wire 	[3:0]	SSD0, SSD1, SSD2, SSD3;
+	wire 	[1:0] ssdscan_clk;
+	
+	assign SSD3 = player_1_score[3:0];
+	assign SSD2 = 4'b1111;
+	assign SSD1 = 4'b1111;
+	assign SSD0 = player_2_score[3:0];
+	
+	// need a scan clk for the seven segment display 
+	// 191Hz (50MHz / 2^18) works well
+	assign ssdscan_clk = DIV_CLK[19:18];	
+	assign An0	= !(~(ssdscan_clk[1]) && ~(ssdscan_clk[0]));  // when ssdscan_clk = 00
+	assign An1	= !(~(ssdscan_clk[1]) &&  (ssdscan_clk[0]));  // when ssdscan_clk = 01
+	assign An2	= !( (ssdscan_clk[1]) && ~(ssdscan_clk[0]));  // when ssdscan_clk = 10
+	assign An3	= !( (ssdscan_clk[1]) &&  (ssdscan_clk[0]));  // when ssdscan_clk = 11
+	
+	
+	always @ (ssdscan_clk, SSD0, SSD1, SSD2, SSD3)
+	begin : SSD_SCAN_OUT
+		case (ssdscan_clk) 
+			2'b00:
+					SSD = SSD0;
+			2'b01:
+					SSD = SSD1;
+			2'b10:
+					SSD = SSD2;
+			2'b11:
+					SSD = SSD3;
+		endcase 
+	end	
+	
+	reg [6:0]  SSD_CATHODES;
+	wire [6:0] 		SSD_CATHODES_blinking;	
+
+	assign SSD_CATHODES_blinking = SSD_CATHODES | ( {7{ q_GAMEOVER & DIV_CLK[25]}} );
+	assign {Ca, Cb, Cc, Cd, Ce, Cf, Cg} = SSD_CATHODES_blinking;
+
+	// and finally convert SSD_num to ssd
+	//reg [6:0]  SSD_CATHODES;
+	//assign {Ca, Cb, Cc, Cd, Ce, Cf, Cg, Dp} = {SSD_CATHODES, 1'b1};
+	// Following is Hex-to-SSD conversion
+	always @ (SSD) 
+	begin : HEX_TO_SSD
+		case (SSD)		
+			4'b1111: SSD_CATHODES = 7'b1111111 ; //Nothing 
+			4'b0000: SSD_CATHODES = 7'b0000001 ; //0
+			4'b0001: SSD_CATHODES = 7'b1001111 ; //1
+			4'b0010: SSD_CATHODES = 7'b0010010 ; //2
+			4'b0011: SSD_CATHODES = 7'b0000110 ; //3
+			4'b0100: SSD_CATHODES = 7'b1001100 ; //4
+			4'b0101: SSD_CATHODES = 7'b0100100 ; //5
+			4'b0110: SSD_CATHODES = 7'b0100000 ; //6
+			4'b0111: SSD_CATHODES = 7'b0001111 ; //7
+			4'b1000: SSD_CATHODES = 7'b0000000 ; //8
+			4'b1001: SSD_CATHODES = 7'b0000100 ; //9
+			4'b1010: SSD_CATHODES = 7'b0001000 ; //10 or A
+			default: SSD_CATHODES = 7'bXXXXXXX ; // default is not needed as we covered all cases
+		endcase
+	end
+	
+	/////////////////////////////////////////////////////////////////
+	//////////////  	  SSD control ends here 	 ///////////////////
+	/////////////////////////////////////////////////////////////////
+endmodule
---
+++ pong_sm.v
@@ -0,0 +1,220 @@
+///////////////////////////////////////////////////////////////////////
+///////		Author: 		Jake Sheppard								///////////
+///////		Date: 			04/16/2017								///////////
+///////		Description:	PONG State Machine					///////////
+///////////////////////////////////////////////////////////////////////
+
+`timescale 1ns/1ps
+
+module pong_sm (clk, reset, start, ack, x, y,text_offset, player_1_score, player_2_score, p_1, p_2, q_INITIAL, q_SERVE, q_MOVE, q_CHECK, q_SCORE, q_GAMEOVER);
+
+	//Inputs
+	input clk, reset, start, ack;
+	input [9:0] p_1;
+	input [9:0] p_2;
+	
+	//Outputs
+	
+	output reg [9:0] x;
+	output reg [9:0] y;
+	output reg [3:0] player_1_score; 
+	output reg [3:0] player_2_score; 
+	output reg [9:0] text_offset;
+	
+	
+	output q_INITIAL, q_SERVE, q_MOVE, q_CHECK, q_SCORE, q_GAMEOVER;
+	reg [5:0] state;
+	assign {q_GAMEOVER, q_SCORE, q_CHECK, q_MOVE, q_SERVE, q_INITIAL} = state;
+	
+
+	reg dx, dy;
+	wire b1, b2, b3, b4, b5;
+	wire s1, s2, s3, s4;
+	
+	reg [9:0] X_step;
+	reg [9:0] Y_step;
+	
+	localparam
+	//paddle size
+	PWIDTH = 10'd15,
+	PHEIGHT = 10'd40,
+	
+	//state assignment
+	INITIAL = 6'b000001,
+	SERVE = 6'b000010,
+	MOVE	= 6'b000100,
+	CHECK	= 6'b001000,
+	SCORE		= 6'b010000,
+	GAMEOVER	= 6'b100000;
+
+	
+	// bounce back conditions
+	assign b1 = ( (x + X_step) >= (10'd640 - PWIDTH) ) &&  ( (y + Y_step) <= (p_2 + PHEIGHT) )  && ( ( (y + Y_step) >= (p_2 - PHEIGHT) ) ) ; // Ball traveling right
+	assign b2 = ( (x - X_step) <= PWIDTH ) &&  ( (y + Y_step) <= (p_1 + PHEIGHT) )  && ( ( (y + Y_step) >= (p_1 - PHEIGHT) ) ) ; // Ball traveling left
+	assign b3 = ( ( (y - Y_step) <= 0 ) || ( (y + Y_step) >= 10'd480 ) ) ; // Top and bottom walls
+	
+	// scoring conditions
+	assign s1 = ( (x + X_step) >= (10'd640) ) && ~( ( (y + Y_step) <= (p_2 + PHEIGHT) )  && ( ( (y + Y_step) >= (p_2 - PHEIGHT) ) ) ) ; // player 1 scores
+	assign s2 = ( (x - X_step) <= 0 ) &&  ~( ( (y + Y_step) <= (p_1 + PHEIGHT) )  && ( ( (y + Y_step) >= (p_1 - PHEIGHT) ) ) ); // player 2 scores
+	
+	
+	//NSL AND SM
+	always @ (posedge clk, posedge reset)
+	begin: pong_SM
+		if (reset)
+		begin
+			state <= INITIAL;
+			player_1_score <= 1'bx;
+			player_2_score <= 1'bx;
+			x <= 10'dx; 
+			y <= 10'dx; 
+			dx <= 1'bx; 
+			dy <= 1'bx; 
+			X_step <= 10'dx;
+			Y_step <= 10'dx;
+			text_offset <= 10'dx;
+		end
+		else
+			case (state)
+				INITIAL:
+				begin
+					//NSL
+					state <= SERVE;
+					//RTL
+					player_1_score <= 1'b0;
+					player_2_score <= 1'b0;
+					x <= 10'd320; //ball in the center
+					y <= 10'd240; 
+					dx <= 1; // ball goes right
+					dy <= 0; // ball goes down
+					X_step <= 10'd12;
+					Y_step <= 10'd6;
+					text_offset <= 10'd0;
+				end
+				SERVE:
+				begin
+					//NSL
+					if ( ( player_1_score == 0 ) && (player_2_score == 0) )
+						begin
+							if (start)
+								state <= MOVE;
+						end
+					else 
+						state <= MOVE;
+					//RTL
+					x <= 10'd320; //ball in the center
+					y <= 10'd240; 
+				end
+				MOVE:
+				begin
+					//NSL
+					state <= CHECK;
+					//RTL
+					if (dx)
+						x <= x + X_step;
+					else
+						x <= x - X_step;
+					
+					if (dy)
+						y <= y - Y_step;
+					else
+						y <= y + Y_step;
+				end
+				CHECK:
+				begin
+					// NSL
+					if ( s1 | s2  )
+						state <= SCORE;
+					else
+						state <= MOVE;
+						
+					//RTL
+					if ( (b1 & dx) ) // ball traveling right towards paddle 2
+						begin
+							dx <= ~dx;
+							if ( (y + Y_step) < (p_2 - 10'd25) ) // top region of paddle
+								begin
+									Y_step <= 12;
+									dy <= 1'b1;
+								end
+							else if ( ( (y + Y_step) < (p_2 - 10'd5) ) && ( (y + Y_step) >= (p_2 - 10'd25) ) ) // 2nd region
+								begin
+									Y_step <= 3;
+									dy <= 1'b1;
+								end
+							else if ( ( (y + Y_step) >= (p_2 - 10'd5) ) && ( (y + Y_step) < (p_2 + 10'd5) ) ) // middle region
+								Y_step <= 0;
+							else if ( ( (y + Y_step) >= (p_2 + 10'd5) ) && ( (y + Y_step) < (p_2 + 10'd25) ) ) // 4th region
+								begin
+									Y_step <= 3;
+									dy <= 1'b0;
+								end
+							else if ( (y + Y_step) >= (p_2 + 10'd25 ) ) // bottom region
+								begin
+									Y_step <= 12;
+									dy <= 1'b0;
+								end
+						end
+					else if ( (b2 & ~dx) ) // ball traveling left towards paddle 1
+						begin
+							dx <= ~dx;
+							if ( (y + Y_step) < (p_1 - 10'd25) ) // top region of paddle
+								begin
+									Y_step <= 12;
+									dy <= 1'b1;
+								end
+							else if ( ( (y + Y_step) < (p_1 - 10'd5) ) && ( (y + Y_step) >= (p_1 - 10'd25) ) ) // 2nd region
+								begin
+									Y_step <= 3;
+									dy <= 1'b1;
+								end
+							else if ( ( (y + Y_step) >= (p_1 - 10'd5) ) && ( (y + Y_step) < (p_1 + 10'd5) ) ) // middle region
+								Y_step <= 0;
+							else if ( ( (y + Y_step) >= (p_1 + 10'd5) ) && ( (y + Y_step) < (p_1 + 10'd25) ) ) // 4th region
+								begin
+									Y_step <= 3;
+									dy <= 1'b0;
+								end
+							else if ( (y + Y_step) >= (p_1 + 10'd25 ) ) // bottom region
+								begin
+									Y_step <= 12;
+									dy <= 1'b0;
+								end
+						end
+					else if ( b3 ) // top or bottom wall
+						dy <= ~dy;
+				end
+				SCORE:
+				begin
+					//NSL
+					if ( ( (player_1_score == 2'b10)& dx ) || ( (player_2_score == 2'b10)& ~dx ) )
+						begin
+						state <= GAMEOVER;
+						if ((player_2_score == 2'b10) & ~dx)
+							text_offset <= 10'd530;
+						end
+					else
+						state <= SERVE;
+					//RTL
+					if ( dx )
+						begin
+							player_1_score <= player_1_score + 2'b01;
+							dx <= ~dx;
+						end
+					else
+						begin
+							player_2_score <= player_2_score + 2'b01;
+							dx <= ~dx;
+						end
+				end
+				GAMEOVER:
+				begin
+					//NSL
+					if (ack)
+						state <= INITIAL;
+						x <= 650; // make ball go out of screen
+						y <= 500;
+				end
+			endcase
+	end
+endmodule
